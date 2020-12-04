8259689c4a788aae7b45b7560d3850c2602efa60
Ant89ne <antoine.bralet@cpe.fr>
2020-12-04 14:10:55 +0100

    size debug"

--- pred.m
+++ pred.m
@@ -1,12 +1,12 @@
 function I_PRED=pred(I, a1, a2, a3)
   
-  I_PRED = zeros(size(I));
-  
+  I_PRED = zeros(s);
+  s = size(I);
   I_PRED(1,:) = I(1,:);
   I_PRED(:,1) = I(:,1);
   
-  for i = 2:size(I)(1)
-    for j = 2:size(I)(2)
+  for i = 2:s(1)
+    for j = 2:s(2)
       I_PRED(i,j) = I(i,j) - (a1*I(i, j-1) + a2*I(i-1, j-1) + a3*I(i-1, j));  
     end
   end
