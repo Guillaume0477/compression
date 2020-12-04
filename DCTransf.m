863fc4f312254cfd836a1f80956456d845341c72
Guillaume <guillaumeduret1@gmail.com>
2020-12-04 16:23:21 +0100

    add dc

--- DCTransf.m
+++ DCTransf.m
@@ -2,9 +2,9 @@ function I_DCT = DCTransf(I)
     s = size(I);
     I_DCT = zeros(s);
     
-    for i = 1:s(0)/8
-      for j = 1:s(1)/8
-        I_DCT(8*(i-1):8*i, 8*j-1:8*j) = dct2(I(8*(i-1):8*i, 8*j-1:8*j));     
+    for i = 1:s(1)/8
+      for j = 1:s(2)/8
+        I_DCT(8*(i-1)+1:8*i, 8*(j-1)+1:8*j) = dct2(I(8*(i-1)+1:8*i, 8*(j-1)+1:8*j));     
       end
     end
     
